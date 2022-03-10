a72362ea301b108381a7418a7961b1bb8153d511
Елизавета <@WIN-C9KJ87ML6PE>
2022-03-11 01:33:25 +0700

    Добавьте файлы проекта.

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edResolution = new System.Windows.Forms.NumericUpDown();
+            this.edDensity = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.edResolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDensity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Разрешение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Плотность";
+            // 
+            // edResolution
+            // 
+            this.edResolution.Location = new System.Drawing.Point(106, 33);
+            this.edResolution.Name = "edResolution";
+            this.edResolution.Size = new System.Drawing.Size(120, 22);
+            this.edResolution.TabIndex = 2;
+            this.edResolution.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // edDensity
+            // 
+            this.edDensity.Location = new System.Drawing.Point(339, 33);
+            this.edDensity.Name = "edDensity";
+            this.edDensity.Size = new System.Drawing.Size(120, 22);
+            this.edDensity.TabIndex = 3;
+            this.edDensity.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(547, 24);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(112, 38);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Пуск";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(715, 24);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(112, 38);
+            this.btStop.TabIndex = 5;
+            this.btStop.Text = "Стоп";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1031, 535);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.edResolution);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btStop);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btStart);
+            this.groupBox1.Controls.Add(this.edDensity);
+            this.groupBox1.Location = new System.Drawing.Point(0, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1031, 98);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 651);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edResolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDensity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edResolution;
+        private System.Windows.Forms.NumericUpDown edDensity;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            startGame();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            stopGame();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nextGeneration();
+        }
+
+        private void stopGame()
+        {
+            if (!timer1.Enabled) return;
+            timer1.Stop();
+            edResolution.Enabled = true;
+            edDensity.Enabled = true;
+        }
+
+        Graphics pucture;
+        int resolution;       
+        int coll;
+        int rows;
+        bool[,] field;
+
+        private void startGame()
+        {
+            if (timer1.Enabled) return;
+
+            edResolution.Enabled = false;
+            edDensity.Enabled = false;
+
+            resolution = (int)edResolution.Value;
+
+            rows = pictureBox1.Height / resolution;
+            coll = pictureBox1.Width / resolution;
+
+            field = new bool[coll, rows];
+
+            Random random = new Random();
+            for (int x = 0; x < coll; x++)
+                for (int y = 0; y < rows; y++) field[x, y] = random.Next((int)edDensity.Value) == 0;
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pucture = Graphics.FromImage(pictureBox1.Image);
+
+            timer1.Start();
+        }
+
+        private void nextGeneration()
+        {
+            pucture.Clear(Color.PapayaWhip);
+
+            var newField = new bool[coll, rows];
+
+            for (int x = 0; x < coll; x++)
+                for (int y = 0; y < rows; y++)
+                {
+                    var neigboursCount = neibCount(x, y);
+                    bool hasLife = field[x, y];
+
+                    if (!hasLife && neigboursCount == 3) newField[x, y] = true;
+                    else if (hasLife && (neigboursCount < 2 || neigboursCount > 3)) newField[x, y] = false;
+                    else newField[x, y] = field[x, y];
+
+                    if (hasLife)
+                        pucture.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                }
+            field = newField;
+            pictureBox1.Refresh();
+        }
+
+        private int neibCount(int x, int y)
+        {
+            int count = 0;
+
+            for (int i = -1; i < 2; i++)
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + coll) % coll;
+                    var row = (y + j + rows) % rows;
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+                    if (hasLife && !isSelfChecking) count++;
+                }
+
+            return count;
+        }
+      
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled) return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+
+                if (validateMousePosition(x, y)) field[x, y] = true;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                if (validateMousePosition(x, y)) field[x, y] = false;
+            }
+        }
+
+        private bool validateMousePosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < coll && y < rows;
+        }
+    }
+}
